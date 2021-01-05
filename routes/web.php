c8d97f95b9ce96158acdc81eaf7a05a02217b854
pamekar <d.pamekar@gmail.com>
2021-01-05 19:55:36 +0100

    Order listed users by ID

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome', ['users' => \App\Models\User::all()]);
+    return view('welcome', ['users' => \App\Models\User::orderBy('id')->get()]);
 });
 
 Route::get('/dashboard', function () {
