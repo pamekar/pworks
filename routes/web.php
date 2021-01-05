9babea6ef9eeef02103875e4b76f087452bb61bb
pamekar <d.pamekar@gmail.com>
2021-01-05 11:08:19 +0100

    List app users on welcome page

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome', ['users' => \App\Models\User::all()]);
 });
 
 Route::get('/dashboard', function () {
