faded4e3526be6147b5afff19cce14935f7df270
pamekar <d.pamekar@gmail.com>
2021-01-05 10:46:43 +0100

    Comments resource route

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,6 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+Route::resource('comments', \App\Http\Controllers\CommentsController::class);
+
 require __DIR__ . '/auth.php';
